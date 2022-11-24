cdf479733aa6978328ab81d77f7dae0198cfe2d9
Mohit Patel <mohit.patel1966@gmail.com>
2022-11-24 17:57:32 +0530

    work kar rha hai guys

--- src/api.js
+++ src/api.js
@@ -1,14 +1,32 @@
-const BASE_API_URL = import.meta.env.VITE_REDIRECT_URI;
+import { ACCESS_TOKEN, logout, TOKEN_TYPE } from "./common";
+
+const BASE_API_URL = import.meta.env.VITE_API_BASE_URL;
 
 const getAccessToken = () => {
 
+    const accessToken = localStorage.getItem(ACCESS_TOKEN);
+    const expiresIn = localStorage.getItem(ACCESS_TOKEN);
+    const tokenType = localStorage.getItem(TOKEN_TYPE);
+    if(Date.now() < expiresIn){
+        return {accessToken , tokenType}
+    }
+    else{
+        logout();
+    }
+}
+
+const createAPIConfig = ({accessToken, tokenType} , method = "GET") => {
+    return {
+        headers: {
+            Authorization: `${tokenType} ${accessToken}`
+        },
+        method
+    }
 }
 
 const fetchRequest = (endpoint) => {
     const url = `${BASE_API_URL}/${endpoint}`;
-    fetch(url,{
-        headers
-    })   
+    fetch(url,createAPIConfig(getAccessToken())) ;
 }
 
 
