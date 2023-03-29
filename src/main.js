026eac0f6d7af61ec5e717b9345c42f2a1449c3c
Mohit Patel <mohit.patel1966@gmail.com>
2023-03-29 13:45:54 +0530

    login page set with index.html

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import './style.css'
 const APP_URL = import.meta.env.VITE_APP_URL;
 
-document.addEventListener("DOMContentLoaded",() => {
-  if(localStorage.getItem("accessToken")){
-    window.location.href = `${APP_URL}/dashboard/dashboard.html`;
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("accessToken")) {
+    window.location.href = `${APP_URL}/dashboard/index.html`;
   }
-  else{
-    window.location.href = `${APP_URL}/login/login.html`;
+  else {
+    window.location.href = `${APP_URL}/login/index.html`;
   }
 })
